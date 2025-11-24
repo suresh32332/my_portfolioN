9e2523dfb16cd4cb101954dbb044fe994784082f
suresh <suresh123@gmail.com>
2025-11-24 16:09:02 +0530

    change the resume

--- src/Component/Navbar/NavbarBtn.jsx
+++ src/Component/Navbar/NavbarBtn.jsx
@@ -6,8 +6,8 @@ const NavbarBtn = () => {
   return (
     <div>
       <a
-        href="/suresh_resume.pdf" // file path from public folder
-        download="Suresh_Resume.pdf" // file name for download
+        href="public/suresh sde.pdf" // file path from public folder
+        download="public/suresh sde.pdf" // file name for download
         className=" text-body px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange  hover:border-orange hover:scale-110 transform-all duration-500 hover:shadow-lg"
       >
         Download Resume
